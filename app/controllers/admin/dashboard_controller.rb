e27caa9707d37c795b935d41ba3dc2b355af1113
Jacquiiii <jacqui.koroll@telus.com>
2023-01-25 17:05:07 -0800

    add product/category count table to dashboard#show

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
 
   def show
+    @product_count = Product.all.count
+    @category_count = Category.all.count
   end
 
 end

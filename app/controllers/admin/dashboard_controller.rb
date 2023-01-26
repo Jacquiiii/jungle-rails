7bbd09ba1cf00f7f1e8c5fa96c4939411db93b52
Jacquiiii <jacqui.koroll@telus.com>
2023-01-25 18:20:31 -0800

    add products and categories links to admin#dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,6 +5,7 @@ class Admin::DashboardController < ApplicationController
   def show
     @product_count = Product.all.count
     @category_count = Category.all.count
+    @categories = Category.all
   end
 
 end

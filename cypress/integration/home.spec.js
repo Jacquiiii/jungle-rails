b687413d2a75701534ed5594902e24bb82555184
Jacquiiii <jacqui.koroll@telus.com>
2023-01-30 19:09:34 -0800

    add tests to cypress home spec

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -8,4 +8,8 @@ describe('example to-do app', () => {
     cy.get(".products article").should("be.visible");
   });
 
+  it("There are 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
 })
